b8e81cbe8911a0e923d97de533d16764c4d2b60f
Hoocken <alexander.anthony.tang@gmail.com>
2026-03-04 19:47:02 +0100

    add MultiEpochsDataLoader

---
+++ dataloader.py
@@ -0,0 +1,35 @@
+from torch.utils import data
+import torch
+
+class MultiEpochsDataLoader(data.DataLoader):
+    """
+    https://discuss.pytorch.org/t/enumerate-dataloader-slow/87778/4
+    Improve loading data per epoch to not recreate it every epoch.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._DataLoader__initialized = False
+        self.batch_sampler = _RepeatSampler(self.batch_sampler)
+        self._DataLoader__initialized = True
+        self.iterator = super().__iter__()
+
+    def __len__(self):
+        return len(self.batch_sampler.sampler)
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield next(self.iterator)
+
+class _RepeatSampler(object):
+    """ Sampler that repeats forever.
+
+    Args:
+        sampler (Sampler)
+    """
+
+    def __init__(self, sampler):
+        self.sampler = sampler
+
+    def __iter__(self):
+        while True:
+            yield from iter(self.sampler)
