89e963f52332cad381aaff6f4f78e5072c38bf35
Hoocken <alexander.anthony.tang@gmail.com>
2026-03-07 13:33:11 +0100

    Add multiprocessing_context

--- dataset.py
+++ dataset.py
@@ -73,7 +73,8 @@ def build_loader(filepath, data_dir, n_speakers, n_utterances, min_seg_length, n
         collate_fn=collate_batch,
         shuffle=True,
         drop_last=True,
-        num_workers=num_workers
+        num_workers=num_workers,
+        multiprocessing_context='spawn',
     )
 
     print('try printing')
@@ -85,7 +86,8 @@ def build_loader(filepath, data_dir, n_speakers, n_utterances, min_seg_length, n
         collate_fn=collate_batch,
         shuffle=True,
         drop_last=True,
-        num_workers=num_workers
+        num_workers=num_workers,
+        multiprocessing_context='spawn'
     )
 
     return train_ld, validation_ld
